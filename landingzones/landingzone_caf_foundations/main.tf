95defb019e0618db2e460086b7f52da196448d3d
Daniel Lochhead <daniellochhead@Daniels-MacBook-Pro.local>
2020-10-28 23:26:25 +0000

    change backend to terraform cloud

--- landingzones/landingzone_caf_foundations/main.tf
+++ landingzones/landingzone_caf_foundations/main.tf
@@ -3,9 +3,19 @@ provider "azurerm" {
 }
 
 terraform {
-  backend "azurerm" {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "dlochhead"
+
+    workspaces {
+      name = "caf-terraform-landingzones"
+    }
   }
 }
+# terraform {
+#   backend "azurerm" {
+#   }
+# }
 
 terraform {
   required_providers {
@@ -27,15 +37,17 @@ locals {
   tags = merge(var.tags, local.landingzone_tag)
 }
 
-data "terraform_remote_state" "launchpad" {
-  backend = "azurerm"
-  config = {
-    storage_account_name = var.lowerlevel_storage_account_name
-    container_name       = var.lowerlevel_container_name
-    key                  = var.lowerlevel_key
-    resource_group_name  = var.lowerlevel_resource_group_name
-  }
-}
+
+
+# data "terraform_remote_state" "launchpad" {
+#   backend = "azurerm"
+#   config = {
+#     storage_account_name = var.lowerlevel_storage_account_name
+#     container_name       = var.lowerlevel_container_name
+#     key                  = var.lowerlevel_key
+#     resource_group_name  = var.lowerlevel_resource_group_name
+#   }
+# }
 
 locals {
   global_settings     = data.terraform_remote_state.launchpad.outputs.global_settings
